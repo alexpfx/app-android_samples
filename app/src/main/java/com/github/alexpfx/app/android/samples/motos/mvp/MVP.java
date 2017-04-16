1ff4afb1974de6454ffca895d22d6e28627d0e1c
Alexandre Alessi <alexpfba@gmail.com>
2017-04-16 11:51:38 -0300

    adicionando rxjava

---
+++ app/src/main/java/com/github/alexpfx/app/android/samples/motos/mvp/MVP.java
@@ -0,0 +1,11 @@
+package com.github.alexpfx.app.android.samples.mvp;
+
+/**
+ * Created by alexandre on 01/04/2017.
+ */
+
+public interface MVP {
+    interface Model {
+
+    }
+}
---
+++ app/src/main/java/com/github/alexpfx/app/android/samples/motos/mvp/Moto.java
@@ -0,0 +1,8 @@
+package com.github.alexpfx.app.android.samples.motos.mvp;
+
+/**
+ * Created by alexandre on 01/04/2017.
+ */
+
+public class Moto {
+}
